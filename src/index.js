ecdc2d0bbd26c216b72b5e19c4b7bc63a05ceb99
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-09-29 18:52:49 +0600

    Add projects slice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import getStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = getStore();
 // subscriber runs until called explicitly
@@ -7,6 +8,7 @@ const unsubscribe = store.subscribe(() =>
   console.log("Store changed to: ", store.getState())
 );
 
+store.dispatch(projectAdded({ name: "Project 1" }));
 store.dispatch(actions.bugAdded({ description: "Bug 1" }));
 store.dispatch(actions.bugAdded({ description: "Bug 2" }));
 store.dispatch(actions.bugResolved({ id: 2 }));
